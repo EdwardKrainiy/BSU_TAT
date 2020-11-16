947f562078688daf587545dc1a30c4a7e2c3b617
Edvard Krainiy <ekrayniy@inbox.ru>
2020-11-16 20:10:14 +0300

    Added CleanCodeLab ver. 1

---
+++ CleanCodeLab/src/main/java/Planes/ExperimentalPlane.java
@@ -0,0 +1,44 @@
+package Planes;
+
+import models.ClassificationLevel;
+import models.ExperimentalTypes;
+
+import java.util.Objects;
+
+public class ExperimentalPlane extends Plane{
+
+    private ExperimentalTypes experimentalType;
+    private ClassificationLevel classificationLevel;
+
+    public ExperimentalPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, ExperimentalTypes type, ClassificationLevel classificationLevel) {
+        super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
+        this.experimentalType = type;
+        this.classificationLevel = classificationLevel;
+    }
+
+    public ClassificationLevel getClassificationLevel(){
+        return classificationLevel;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ExperimentalPlane)) return false;
+        if (!super.equals(o)) return false;
+        ExperimentalPlane that = (ExperimentalPlane) o;
+        return experimentalType == that.experimentalType &&
+                getClassificationLevel() == that.getClassificationLevel();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), experimentalType, classificationLevel);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString().replace("}",
+                ", type=" + experimentalType +
+                        ", classificationLevel" + classificationLevel);
+    }
+}
