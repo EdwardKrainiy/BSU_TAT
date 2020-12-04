03578619de3c95576abf322ebc80e9e6d5e615c9
Edvard Krainiy <ekrayniy@inbox.ru>
2020-12-04 14:53:26 +0300

    WebDriver lab was changed to PageObject

---
+++ Automation/src/test/java/XiaomiStoreTest.java
@@ -0,0 +1,53 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import page.LandingPage;
+import page.SearchPage;
+
+import java.util.concurrent.TimeUnit;
+
+public class XiaomiStoreTest {
+     WebDriver driver;
+     SearchPage searchPage;
+     LandingPage landingPage;
+
+     @BeforeClass
+     public void prepareAll(){
+          System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+
+          driver = new ChromeDriver();
+          searchPage = new SearchPage(driver);
+          landingPage = new LandingPage(driver);
+
+          driver.manage().window().maximize();
+          driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+     }
+
+     @Test
+     public void testSearchAllItemsWithXiaomiString() throws InterruptedException {
+          driver.get("https://xistore.by/");
+
+          landingPage.clickCloseAdButton();
+
+          landingPage.clickCloseChatButton();
+
+          landingPage.inputStringToSearchField("Xiaomi");
+
+          landingPage.clickSearchButton();
+
+          Assert.assertEquals("Xiaomi", landingPage.getNameOfTheFirstPhoneOnPage());
+     }
+
+     @Test(dependsOnMethods = "testSearchAllItemsWithXiaomiString")
+     public void testAddOnePhoneToBasket(){
+          driver.get("https://xistore.by/search/?q=Xiaomi");
+
+          searchPage.clickOnTheFirstPhoneLink();
+
+          searchPage.clickAddToBasketButton();
+
+          Assert.assertEquals("1", searchPage.getAmountOfItemsInBasket());
+     }
+}
---
+++ Automation/src/test/java/page/LandingPage.java
@@ -0,0 +1,67 @@
+package page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LandingPage {
+
+    private WebDriver driver;
+
+    public LandingPage(WebDriver driver){
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
+    @FindBy(xpath = "//*[@id=\"title-search-input\"]")
+    public WebElement searchField;
+
+    @FindBy(xpath = "//*[@id=\"title-search\"]/form/button")
+    public WebElement searchButton;
+
+    public WebElement getCloseAdButton(){
+        try {
+            return new WebDriverWait(driver, 5).until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.className("popmechanic-close"))).get(0);
+        } catch (Exception e){
+            System.out.println("Ad not found.");
+            return null;
+        }
+    }
+
+    public void clickCloseAdButton(){
+        if(getCloseAdButton() != null){
+            getCloseAdButton().click();
+        }
+    }
+
+    public WebElement getCloseChatButton(){
+        try {
+            return new WebDriverWait(driver, 5).until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.className("support-close"))).get(0);
+        } catch (Exception e){
+            System.out.println("Support chat not found.");
+            return null;
+        }
+    }
+
+    public void clickCloseChatButton(){
+        if(getCloseChatButton() != null){
+            getCloseChatButton().click();
+        }
+    }
+
+    public void inputStringToSearchField(String query){
+        searchField.sendKeys(query);
+    }
+
+    public void clickSearchButton(){
+        searchButton.click();
+    }
+
+    public String getNameOfTheFirstPhoneOnPage(){
+        return this.driver.findElement(By.xpath("//*[@id=\"bx_3966226736_10342\"]/div/a[2]/b")).getText();
+    }
+}
