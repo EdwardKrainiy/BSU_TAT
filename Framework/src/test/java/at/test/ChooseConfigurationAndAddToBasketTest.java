5c6c8817673cb324cc18d87e9213fd1bcd1bb6f8
Фицык <47747074+EdwardKrainiy@users.noreply.github.com>
2020-12-11 01:16:44 +0300

    Update ChooseConfigurationAndAddToBasketTest.java

--- Framework/src/test/java/at/test/ChooseConfigurationAndAddToBasketTest.java
+++ Framework/src/test/java/at/test/ChooseConfigurationAndAddToBasketTest.java
@@ -14,6 +14,31 @@ public class ChooseConfigurationAndAddToBasketTest extends CommonCondition {
                 .getAmountOfPhones();
         Assert.assertEquals(amountOfPhonesOnBasket, 1);
     }
+    
+    @Test
+    public void AddingAndSubtractingAmountOfPhoneTest(){
+        BuyingPage buyingPage = new BuyingPage(driver);
+        int amountOfPhonesOnBasketBeforeAddingAndSubtracting = buyingPage
+                .openPage()
+                .buyPhone()
+                .addToBasket()
+                .getAmountOfPhones();
+        int amountOfPhonesOnBasketAfterAddingAndSubtracting = buyingPage
+                .clickOnMinusButton()
+                .clickOnPlusButton()
+                .getAmountOfPhones();
+        Assert.assertEquals(amountOfPhonesOnBasketBeforeAddingAndSubtracting, amountOfPhonesOnBasketAfterAddingAndSubtracting);
+    }
 
+    @Test
+    public void DeletingPhoneFromBasket(){
+        String basketIsEmptyText = new BuyingPage(driver)
+                .openPage()
+                .buyPhone()
+                .addToBasket()
+                .deletePhoneFromBasket()
+                .getBasketIsEmptyText();
+        Assert.assertEquals(basketIsEmptyText, "Вы ничего не добавили в корзину");
+    }
     
 }
