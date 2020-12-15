adc1cb51df76f991d7e690b97768d158564a41d8
Edvard Krainiy <ekrayniy@inbox.ru>
2020-12-15 16:34:28 +0300

    Minor changes on xpathes

--- FrameworkTask/src/test/java/page/ComparingPage.java
+++ FrameworkTask/src/test/java/page/ComparingPage.java
@@ -14,13 +14,13 @@ public class ComparingPage extends AbstractPage {
     private final By listOfPhonesOnComparingPage =  By.className("mCS_img_loaded");
     Logger log = Logger.getLogger(ComparingPage.class);
 
-    @FindBy(xpath = "//*[@id=\"bx_1717408340_10342\"]/div[2]/a/span[1]")
+    @FindBy(className = "js_compare_id_10342")
     private WebElement compareFirstPhoneButton;
 
-    @FindBy(xpath = "//*[@id=\"bx_1717408340_25893\"]/div[2]/a/span[1]")
+    @FindBy(className = "js_compare_id_25893")
     private WebElement compareSecondPhoneButton;
 
-    @FindBy(xpath = "/html/body/div[2]/footer/div[3]/a[1]")
+    @FindBy(className = "comparison-mob__btn")
     private WebElement sendToComparingPageButton;
 
     @FindBy(className = "remove-item")
